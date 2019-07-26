d41522b1b933353de64ecfd04cd3125d7a4eee77
rickyhu <rickyhuyujia@gmail.com>
2019-07-26 21:02:16 +0800

    BAC001

---
+++ spring-boot-starter-master/src/main/java/com/wego/controller/SkinController.java
@@ -0,0 +1,29 @@
+package com.wego.controller;
+
+import com.wego.model.ResultModel;
+import com.wego.service.SkinServer;
+import com.wego.service.UserServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author elizayuan
+ * 主要用于用户的接口
+ */
+@RestController
+public class SkinController {
+    @Autowired
+    SkinServer skinServer;
+
+    /**
+     * 根据用户的id，宠物id，食物id喂食，同时增在宠物的col
+     * @param uid
+     * @return
+     */
+    @PostMapping("user/buyskin")
+    ResultModel<Integer> buyskin(@RequestParam Integer uid, @RequestParam Integer sid) {
+        return skinServer.buySkin(uid,sid);
+    }
+}
