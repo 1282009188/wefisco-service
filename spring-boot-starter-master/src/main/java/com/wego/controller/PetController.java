1e95a9753f72736784ba6de2a9a5262731c80d16
rickyhu <rickyhuyujia@gmail.com>
2019-07-27 23:04:58 +0800

    change pet name

--- spring-boot-starter-master/src/main/java/com/wego/controller/PetController.java
+++ spring-boot-starter-master/src/main/java/com/wego/controller/PetController.java
@@ -4,9 +4,7 @@ import com.wego.entity.Pet;
 import com.wego.model.ResultModel;
 import com.wego.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,11 +20,26 @@ public class PetController {
 
     /**
      * 在商城中查询所有的宠物
+     *
      * @return
      */
     @GetMapping("market/pet")
     @ResponseBody
-    ResultModel<HashMap<String,List<Pet>>> marketPet(){
+    ResultModel<HashMap<String, List<Pet>>> marketPet() {
         return petService.showMarketPet();
     }
+
+    /**
+     * 在商城中查询所有的宠物
+     *
+     * @return
+     */
+    @PostMapping("pet/changename")
+    @ResponseBody
+    @CrossOrigin(origins = "*")
+    ResultModel changePet(@RequestParam int uid, @RequestParam int pid, @RequestParam String name) {
+        return petService.changePet(uid, pid, name);
+    }
+
+
 }
--- spring-boot-starter-master/src/main/java/com/wego/service/impl/PetServiceImpl.java
+++ spring-boot-starter-master/src/main/java/com/wego/service/impl/PetServiceImpl.java
@@ -1,7 +1,10 @@
 package com.wego.service.impl;
 
 import com.wego.dao.PetMapper;
+import com.wego.dao.UserMapper;
+import com.wego.dao.UserPetMapper;
 import com.wego.entity.Pet;
+import com.wego.entity.UserPet;
 import com.wego.model.ResultModel;
 import com.wego.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +17,13 @@ import java.util.List;
 public class PetServiceImpl implements PetService {
     @Autowired
     PetMapper petMapper;
+    @Autowired
+    UserPetMapper userPetMapper;
+    @Autowired
+    UserMapper userMapper;
 
-    public ResultModel<HashMap<String,List<Pet>>> showMarketPet() {
-        ResultModel<HashMap<String,List<Pet>>> resultModel = new ResultModel<>();
+    public ResultModel<HashMap<String, List<Pet>>> showMarketPet() {
+        ResultModel<HashMap<String, List<Pet>>> resultModel = new ResultModel<>();
         List<Pet> pets = petMapper.selectAll();
         if (pets == null || pets.size() == 0) {
             resultModel.setCode(1);
@@ -25,10 +32,21 @@ public class PetServiceImpl implements PetService {
         }
         resultModel.setCode(0);
         resultModel.setMessage("查询成功");
-        HashMap<String,List<Pet>> hashMap=new HashMap<>();
-        hashMap.put("petlist",pets);
+        HashMap<String, List<Pet>> hashMap = new HashMap<>();
+        hashMap.put("petlist", pets);
         resultModel.setData(hashMap);
         return resultModel;
 
+    }
+
+    @Override
+    public ResultModel changePet(int uid, int pid, String name) {
+        ResultModel resultModel = new ResultModel();
+        UserPet up = userPetMapper.selectByUidAndPid(uid, pid);
+        up.setPname(name);
+        userPetMapper.updateByPrimaryKey(up);
+        resultModel.setCode(0);
+        resultModel.setMessage("修改成功");
+        return resultModel;
     }
 }
